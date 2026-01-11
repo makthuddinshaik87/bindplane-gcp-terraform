a3e3ae5c6d8e90fa208837cdac8bed5ef9695e1c
makthuddinshaik87 <makthuddin.shaik87@gmail.com>
2026-01-11 21:34:26 +0530

    Update terraform_sep_logs.tf

--- terraform_sep_logs.tf
+++ terraform_sep_logs.tf
@@ -10,9 +10,9 @@ provider "bindplane" {
 ################################
 # SOURCES (Unique Names)
 ################################
-resource "bindplane_source" "host_metrics_v4" {
+resource "bindplane_source" "host_metrics_v5" {
   rollout = true
-  name    = "host-metrics-copy-v3"
+  name    = "host-metrics-copy-v5"
   type    = "host"
 
   parameters_json = jsonencode([
@@ -27,18 +27,18 @@ resource "bindplane_source" "host_metrics_v4" {
   ])
 }
 
-resource "bindplane_source" "journald_logs_v4" {
+resource "bindplane_source" "journald_logs_v5" {
   rollout = true
-  name    = "journald-logs-copy-v4"
+  name    = "journald-logs-copy-v5"
   type    = "journald"
 }
 
 ################################
 # PROCESSOR (Unique Name)
 ################################
-resource "bindplane_processor" "batch_v4" {
+resource "bindplane_processor" "batch_v5" {
   rollout = true
-  name    = "batch-copy-v4"
+  name    = "batch-copy-v5"
   type    = "batch"
 
   parameters_json = jsonencode([
@@ -60,9 +60,9 @@ resource "bindplane_processor" "batch_v4" {
 ################################
 # CONFIGURATION: LOGS
 ################################
-resource "bindplane_configuration" "logs_config_v4" {
+resource "bindplane_configuration" "logs_config_v5" {
   rollout  = true
-  name     = "bindplane-agent-vm-logs-v4"
+  name     = "bindplane-agent-vm-logs-v5"
   platform = "linux"
 
   labels = {
@@ -71,8 +71,8 @@ resource "bindplane_configuration" "logs_config_v4" {
   }
 
   source {
-    name       = bindplane_source.journald_logs_v4.name
-    processors = [bindplane_processor.batch_v4.name]
+    name       = bindplane_source.journald_logs_v5.name
+    processors = [bindplane_processor.batch_v5.name]
   }
 
   destination {
@@ -83,9 +83,9 @@ resource "bindplane_configuration" "logs_config_v4" {
 ################################
 # CONFIGURATION: METRICS
 ################################
-resource "bindplane_configuration" "metrics_config_v4" {
+resource "bindplane_configuration" "metrics_config_v5" {
   rollout  = true
-  name     = "bindplane-agent-vm-metrics-v4"
+  name     = "bindplane-agent-vm-metrics-v5"
   platform = "linux"
 
   labels = {
@@ -94,8 +94,8 @@ resource "bindplane_configuration" "metrics_config_v4" {
   }
 
   source {
-    name       = bindplane_source.host_metrics_v4.name
-    processors = [bindplane_processor.batch_v4.name]
+    name       = bindplane_source.host_metrics_v5.name
+    processors = [bindplane_processor.batch_v5.name]
   }
 
   destination {
